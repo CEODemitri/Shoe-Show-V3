1d3f602c1725c651729a287229dd6e16a360fdd6
ceoDemitri <demichole@gmail.com>
2024-06-30 17:48:37 -0500

    fixed hero responsivness

--- src/app/Pages/Calendar/page.js
+++ src/app/Pages/Calendar/page.js
@@ -7,12 +7,12 @@ export default function Collections() {
     <main>
       <div className="min-h-[92vh] grid md:grid-cols-12 grid-rows-12 gap-3 m-2 font-mono">
         {/* hero section */}
-        <div className="md:col-span-12 row-span-5 bg-red-500 rounded-md p-4 flex flex-col md:flex-row">
+        <div className="md:col-span-12 row-span-5 bg-red-500 rounded-md p-4 flex flex-col md:flex-row max-w-[1040px">
           {/* left hero */}
-          <article className="w-full h-[350px] md:w-1/2 md:h-full bg-red-950"></article>
+          <article className="w-full h-[350px] md:w-1/2 md:h-full bg-red-950 max-w-[600px] m-auto"></article>
 
           {/* right hero */}
-          <article className="w-full h-[400px] md:w-1/2 md:h-full bg-red-100"></article>
+          <article className="w-full h-[400px] md:w-1/2 md:h-full bg-red-100 max-w-[600px] m-auto"></article>
         </div>
       </div>
     </main>
