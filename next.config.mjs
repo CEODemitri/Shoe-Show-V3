299caace661831f5a354875578c270e5cb8eb210
ceoDemitri <demichole@gmail.com>
2024-07-20 22:51:51 -0500

    calendar page near completion. filling in card info from db

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,21 @@ const nextConfig = {
   images: {
     domains: ["cloud.appwrite.io"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
