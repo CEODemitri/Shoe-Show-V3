8e9bbb0a4d2db75d4689b3c1028d80b0b060a9f7
ceoDemitri <demichole@gmail.com>
2025-06-06 06:45:45 -0500

    version four branch created\ Starting from a new branch with fresh slate. readme updated and retrospective folder added with logs.

--- README.md
+++ README.md
@@ -1,51 +1,8 @@
-# the new season begins
-## version 4
-instead of starting from scratch, a perfect time to practice my refactoring an old code base and making valuable upgrades.
+# season 4
 
-### sprint 1
-- better copy
-- design system and page
-- why do i have the pages in a pages folder?
+built in Figma
+built with NextJS, Tailwind, 
 
-
----
-<img width="1306" alt="shoe3" src="https://github.com/user-attachments/assets/e10c38a5-cd65-474b-8e35-8a9a410e1764">
-
-# Silly Me
-
-Version 3 after a failed ruby/rails deploy attempt, and one vanilla js.
-I recreated this site for the third time because I have a vision in my head of what this site could be. My skills are approaching my idea and using this project as a measure of my Full Stack skills.
-
-## Front End features
-
-- [ ] Responsive Design
-- [x] Custom Components
-- [ ] State Management
-- [ ] API Intergration
-- [ ] Animation and Interactivity
-- [ ] Accessibility
-- [ ] Performance Optimization
-
-## Back End features
-
-- [ ] Dynamic Content w/ CMS
-- [ ] Database Intergration
-- [ ] API Intergration
-- [ ] Performance Monitoring and Anaylytics
-- [ ] Authentication
-
----
-
-## Tools
-
-1. NextJS
-2. BunJS
-3. TailwindCSS
-4. FontSource
-5. GreenSock (GSAP)
-6. React Icons
-
-todo :: delete after every session
-
-0.  complete charity/page comment in 2nd div to complete header
-1.  complete navbar comment, hamburger menu
+## agenda
+0. fix links. charity is now community
+1. add utility styles in design and site css
--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -31,39 +31,39 @@ export default function NavBar() {
           <span className="title text-[#232230]">SHOE SHOW</span>
         </a>
 
-        <section className="flex gap-5 items-center">
-          <div className="hidden md:flex gap-8">
-            {links.map((link) => (
-              <Link
-                href={link.to}
-                key={link.id}
-                className="hover:bg-blue-900 hover:text-white hover:px-4 hover:py-1 hover:scale-110 hover:rounded-md duration-200"
-              >
-                {link.name}
-              </Link>
-            ))}
-          </div>
+        {/*<section className="flex gap-5 items-center">*/}
+        {/*  <div className="hidden md:flex gap-8">*/}
+        {/*    {links.map((link) => (*/}
+        {/*      <Link*/}
+        {/*        href={link.to}*/}
+        {/*        key={link.id}*/}
+        {/*        className="hover:bg-blue-900 hover:text-white hover:px-4 hover:py-1 hover:scale-110 hover:rounded-md duration-200"*/}
+        {/*      >*/}
+        {/*        {link.name}*/}
+        {/*      </Link>*/}
+        {/*    ))}*/}
+        {/*  </div>*/}
 
-          {/* on press, open hamburger menu */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="20"
-            height="20"
-            viewBox="0 0 50 50"
-            className="m-auto md:hidden"
-          >
-            <path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>
-          </svg>
+        {/*  /!* on press, open hamburger menu *!/*/}
+        {/*  <svg*/}
+        {/*    xmlns="http://www.w3.org/2000/svg"*/}
+        {/*    x="0px"*/}
+        {/*    y="0px"*/}
+        {/*    width="20"*/}
+        {/*    height="20"*/}
+        {/*    viewBox="0 0 50 50"*/}
+        {/*    className="m-auto md:hidden"*/}
+        {/*  >*/}
+        {/*    <path d="M 0 9 L 0 11 L 50 11 L 50 9 Z M 0 24 L 0 26 L 50 26 L 50 24 Z M 0 39 L 0 41 L 50 41 L 50 39 Z"></path>*/}
+        {/*  </svg>*/}
 
-          <a
-            href="#"
-            className="bg-[#F9C80E] px-6 py-1 rounded-md text-[#2C2B3C] title drop-shadow-sm"
-          >
-            Sign In
-          </a>
-        </section>
+        {/*  <a*/}
+        {/*    href="#"*/}
+        {/*    className="bg-[#F9C80E] px-6 py-1 rounded-md text-[#2C2B3C] title drop-shadow-sm"*/}
+        {/*  >*/}
+        {/*    Sign In*/}
+        {/*  </a>*/}
+        {/*</section>*/}
       </nav>
     </>
   );
